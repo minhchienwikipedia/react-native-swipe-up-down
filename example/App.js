97e5f19a0530c4f36d3e7c31b63f1d588797a847
Frieder Bluemle <frieder.bluemle@gmail.com>
2019-03-21 19:31:30 +0800

    Update example app UI
    This also makes it possible to set the animation at runtime.

--- example/App.js
+++ example/App.js
@@ -1,31 +1,54 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Picker } from 'react-native';
 import SwipeUpDown from 'react-native-swipe-up-down';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      animation: "easeInEaseOut"
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text onPress={() => this.swipeUpDownRef.showFull()}>
-          {' '}
-          Welcome to React Native!{' '}
+        <Text style={styles.welcome}>Welcome to{'\n'}react-native-swipe-up-down</Text>
+        <Text style={{ margin: 20 }} onPress={() => this.swipeUpDownRef.showFull()}>
+          Tap to open panel
         </Text>
+        <Picker
+          selectedValue={this.state.animation}
+          style={{ width: 200 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({animation: itemValue})
+          }>
+          <Picker.Item label="linear" value="linear"/>
+          <Picker.Item label="spring" value="spring"/>
+          <Picker.Item label="easeInEaseOut" value="easeInEaseOut"/>
+          <Picker.Item label="none" value="none"/>
+        </Picker>
         <SwipeUpDown
           hasRef={ref => (this.swipeUpDownRef = ref)}
-          // itemMini={
-          //   <Text style={styles.welcome}>Welcome to React Native!</Text>
-          // }
+          itemMini={
+            <View style={{ alignItems: 'center' }}>
+              <Text>This is the mini view, swipe up!</Text>
+            </View>
+          }
           itemFull={
-            <Text style={styles.instructions}>
-              Welcome to component {'\n'} Swipe Up Down on React Native
-            </Text>
+            <View style={styles.panelContainer}>
+              <Text style={styles.instructions}>
+                Swipe down to close
+              </Text>
+            </View>
           }
           onShowMini={() => console.log('mini')}
           onShowFull={() => console.log('full')}
           disablePressToShow={false}
           style={{ backgroundColor: '#ccc' }}
-          animation="easeInEaseOut"
+          animation={this.state.animation}
         />
       </View>
     );
@@ -37,16 +60,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    marginTop: -5
+    marginTop: 10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
+  panelContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
 });
